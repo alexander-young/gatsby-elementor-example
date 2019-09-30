a5b58cb288dd9fefebfb1cbe7e2b01a21420e126
Alex Y <alx.young234@gmail.com>
2019-09-30 09:19:38 -0600

    Update image.js

--- image.js
+++ image.js
@@ -1,7 +1,11 @@
 import React from "react"
+import Img from "gatsby-image"
 
 const Image = (settings) => {
-  console.log({ settings });
-  return (<div dangerouslySetInnerHTML={{ __html: settings.image.tag }} />)
+  // console.log({ settings });
+  console.log(settings.image.fluid);
+  // return (<div dangerouslySetInnerHTML={{ __html: settings.image.tag }} />)
+  // return (<Img fluid={settings.image.fluid} />)
+  return (<Img fluid={settings.image.fluid} />)
 };
 export default Image;
